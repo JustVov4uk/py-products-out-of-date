2162ccc788f7fc4e9ce19f879c985eba13686090
JustVov4uk <volodabudzan4@gmail.com>
2025-01-09 14:06:20 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,11 +1,12 @@
-import pytest
+from unittest.mock import MagicMock
+from unittest.mock import patch
 import datetime
 from app.main import outdated_products
-from unittest.mock import patch
 
 
-@patch("app.main.datetime.date.today", return_value = datetime.date(2022, 2, 2))
-def test_outdated_products_case_1():
+def test_when_there_are_bad_products() -> None:
+    mocked_date = MagicMock()
+    mocked_date.date.today.return_value = datetime.date(2022, 2, 2)
     products = [
         {"name": "salmon",
          "expiration_date": datetime.date(2022, 2, 10),
@@ -19,5 +20,57 @@ def test_outdated_products_case_1():
          "expiration_date": datetime.date(2022, 2, 1),
          "price": 160},
     ]
-    result = outdated_products(products)
-    assert result == ["duck"]
+    with patch("app.main.datetime", mocked_date):
+        result = outdated_products(products)
+        assert result == ["duck"]
+
+
+def test_when_all_products_are_good() -> None:
+    mocked_date = MagicMock()
+    mocked_date.date.today.return_value = datetime.date(2022, 2, 2)
+    products = [
+        {"name": "salmon",
+         "expiration_date": datetime.date(2022, 2, 10),
+         "price": 600},
+
+        {"name": "chicken",
+         "expiration_date": datetime.date(2022, 2, 5),
+         "price": 120},
+
+        {"name": "duck",
+         "expiration_date": datetime.date(2022, 2, 3),
+         "price": 160},
+    ]
+    with patch("app.main.datetime", mocked_date):
+        result = outdated_products(products)
+        assert result == []
+
+
+def test_limit_test() -> None:
+    mocked_date = MagicMock()
+    mocked_date.date.today.return_value = datetime.date(2022, 2, 2)
+    products = [
+        {"name": "salmon",
+         "expiration_date": datetime.date(2022, 2, 2),
+         "price": 600},
+
+        {"name": "chicken",
+         "expiration_date": datetime.date(2022, 2, 1),
+         "price": 120},
+
+        {"name": "duck",
+         "expiration_date": datetime.date(2022, 2, 2),
+         "price": 160},
+    ]
+    with patch("app.main.datetime", mocked_date):
+        result = outdated_products(products)
+        assert result == ["chicken"]
+
+
+def test_empty_list() -> None:
+    mocked_date = MagicMock()
+    mocked_date.date.today.return_value = datetime.date(2022, 2, 2)
+    products = []
+    with patch("app.main.datetime", mocked_date):
+        result = outdated_products(products)
+        assert result == []
